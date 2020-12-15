6b2d02f6ea9ffc59fdfe1a78fe674155aa1dacc0
Beth-A <elizabeth78@me.com>
2020-12-15 12:33:30 -0500

    updated project

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -7,16 +7,41 @@
 //
 
 import UIKit
+import CLTypingLabel
 
 class WelcomeViewController: UIViewController {
-
-    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var titleLabel: CLTypingLabel!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.navigationController?.isNavigationBarHidden = true
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.navigationController?.isNavigationBarHidden = false
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       
+        
+        //Cocoapods Animation of Title
+        titleLabel.text = K.appName
+        
+        
+        //   Manual Animation of Title
+        //        titleLabel.text = ""
+        //        var charIndex = 0.0
+        //        let titleText = "⚡️FlashChat"
+        //        for letter in titleText {
+        //            print(charIndex)
+        //            print(letter)
+        //            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { (timer) in
+        //                self.titleLabel.text?.append(letter)
+        //            }
+        //            charIndex += 1
+        //        }
+        
     }
     
-
+    
 }
---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,15 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Mary Arnold on 7/12/20.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+
+struct Message {
+    let sender: String //user
+    let body: String //content of message
+}
